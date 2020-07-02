a9f3f440819873df43b8251fbbb712278f8ba7a0
bugbit <ohernandezba@gmail.com>
2020-07-02 18:59:26 +0200

    console

--- NetCoreRobots.Console/Program.cs
+++ NetCoreRobots.Console/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Text;
+using static System.Console;
 
 namespace NetCoreRobots.Console
 {
+    // https://en.wikipedia.org/wiki/Box-drawing_character
     class Program
     {
+        const int StatusBoxesWitdh = 20;
+        const int SepFieldStatusBoxesWitdh = 3;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var x = LargestWindowWidth;
+
+            SetWindowSize(x, 60);
+            BufferWidth = x;
+            BufferHeight = 60;
+            //OutputEncoding = Encoding.GetEncoding(28591);
+
+            //for (var i = 0; i < 512; i++)
+            //    Write($"{i} = {(char)i}\t");
+
+            DisplayScreen();
+            ReadLine();
+        }
+
+        static void DisplayScreen()
+        {
+            var pCursor = CursorVisible;
+
+            CursorVisible = false;
+            try
+            {
+                Clear();
+                DisplayField();
+            }
+            finally
+            {
+                CursorVisible = pCursor;
+            }
+        }
+
+        static void DisplayField()
+        {
+            var pWidth = BufferWidth - StatusBoxesWitdh - SepFieldStatusBoxesWitdh;
+            var pHeigh = BufferHeight;
+            var pW1 = pWidth - 1;
+            var pH2 = pHeigh - 2;
+
+            SetCursorPosition(0, 0);
+            Write('┌');
+            for (var x = 1; x < pW1; x++)
+                Write('─');
+            WriteLine('┐');
+            for (var y = 1; y < pH2; y++)
+            {
+                Write('│');
+                SetCursorPosition(pW1, y);
+                WriteLine('│');
+            }
+            Write('└');
+            for (var x = 1; x < pW1; x++)
+                Write('─');
+            WriteLine('┘');
         }
     }
 }
